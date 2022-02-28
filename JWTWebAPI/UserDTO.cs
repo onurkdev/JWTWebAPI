becfb3c90bb28c418813e5fef9907c183c0aff45
OnurK.Dev <88968938+onurkdev@users.noreply.github.com>
2022-02-28 17:16:59 +0300

    Add project files.

---
+++ JWTWebAPI/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTWebAPI
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
